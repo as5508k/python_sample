7587875c51e9fcefd1c19123b9bce7295d3cbac7
Ashik Saji <81752813+as5508k@users.noreply.github.com>
2021-04-01 17:39:35 +0100

    Simple Python Addition Calculator
    Simple Python Code

---
+++ Sample_code.py
@@ -0,0 +1,8 @@
+print("SIMPLE ADDITION CALCULATOR")
+a = int(input("Enter the first digit:- "))
+b = int(input("Enter the second digit:- "))
+
+def add(a,b):
+    return a+b
+
+print("The Added Answer is: ", add(a,b))
